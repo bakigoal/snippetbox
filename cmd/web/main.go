0312f7269f7eaf8d1970d287fc24467568acc490
Ilmir Bakirov <bakirov@dev.vtb.ru>
2023-06-19 09:40:53 +0300

    refactoring

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -17,21 +17,11 @@ func main() {
 	flag.Parse()
 
 	app := createApplication()
-
-	mux := http.NewServeMux()
-
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-
-	mux.HandleFunc("/", app.home)
-	mux.HandleFunc("/snippet/view", app.snippetView)
-	mux.HandleFunc("/snippet/create", app.snippetCreate)
-
 	app.infoLog.Printf("Starting server on %s", *addr)
 	srv := &http.Server{
 		Addr:     *addr,
 		ErrorLog: app.errorLog,
-		Handler:  mux,
+		Handler:  getHandler(app),
 	}
 	err := srv.ListenAndServe()
 	app.errorLog.Fatal(err)
@@ -42,4 +32,16 @@ func createApplication() *application {
 		infoLog:  log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime),
 		errorLog: log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile),
 	}
+}
+
+func getHandler(app *application) http.Handler {
+	mux := http.NewServeMux()
+
+	fileServer := http.FileServer(http.Dir("./ui/static/"))
+	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+
+	mux.HandleFunc("/", app.home)
+	mux.HandleFunc("/snippet/view", app.snippetView)
+	mux.HandleFunc("/snippet/create", app.snippetCreate)
+	return mux
 }
