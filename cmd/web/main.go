b90cad55c23f1957b40458097b05721e70ee06de
Ilmir Bakirov <bakirov@dev.vtb.ru>
2023-06-19 09:42:49 +0300

    refactoring

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -21,7 +21,7 @@ func main() {
 	srv := &http.Server{
 		Addr:     *addr,
 		ErrorLog: app.errorLog,
-		Handler:  getHandler(app),
+		Handler:  routes(app),
 	}
 	err := srv.ListenAndServe()
 	app.errorLog.Fatal(err)
@@ -32,16 +32,4 @@ func createApplication() *application {
 		infoLog:  log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime),
 		errorLog: log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile),
 	}
-}
-
-func getHandler(app *application) http.Handler {
-	mux := http.NewServeMux()
-
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
-
-	mux.HandleFunc("/", app.home)
-	mux.HandleFunc("/snippet/view", app.snippetView)
-	mux.HandleFunc("/snippet/create", app.snippetCreate)
-	return mux
 }
