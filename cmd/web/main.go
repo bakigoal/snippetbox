dcc6b9a8396ada8e6fc81cc7a88d9304bc4917df
Ilmir Bakirov <bakirov@dev.vtb.ru>
2023-07-04 17:31:34 +0300

    add userModel

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -20,6 +20,7 @@ type application struct {
 	infoLog        *log.Logger
 	errorLog       *log.Logger
 	snippets       *models.SnippetModel
+	users          *models.UserModel
 	templateCache  map[string]*template.Template
 	formDecoder    *form.Decoder
 	sessionManager *scs.SessionManager
@@ -49,6 +50,7 @@ func main() {
 		infoLog:        log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime),
 		errorLog:       log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile),
 		snippets:       &models.SnippetModel{DB: db},
+		users:          &models.UserModel{DB: db},
 		templateCache:  templateCache,
 		formDecoder:    form.NewDecoder(),
 		sessionManager: initSessionManager(db),
