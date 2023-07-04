39fc760b0cd22bb6f9625c92e48bfa51595a89b8
Ilmir Bakirov <bakirov@dev.vtb.ru>
2023-07-04 18:14:07 +0300

    insert user to db and validate email duplication

--- internal/models/users.go
+++ internal/models/users.go
@@ -1,7 +1,10 @@
 package models
 
 import (
+	"context"
 	"github.com/jackc/pgx/v5/pgxpool"
+	"golang.org/x/crypto/bcrypt"
+	"strings"
 	"time"
 )
 
@@ -18,6 +21,23 @@ type UserModel struct {
 }
 
 func (m *UserModel) Insert(name, email, password string) error {
+	hashedPassword, err := bcrypt.GenerateFromPassword([]byte(password), 12)
+	if err != nil {
+		return err
+	}
+
+	stmt := `
+INSERT INTO snippetbox.users (name, email, hashed_password, created)
+VALUES ($1, $2, $3, $4)
+`
+	createTime := time.Now()
+	_, err = m.DB.Exec(context.Background(), stmt, name, email, string(hashedPassword), createTime)
+	if err != nil {
+		if strings.Contains(err.Error(), "users_uc_email") {
+			return ErrDuplicateEmail
+		}
+		return err
+	}
 	return nil
 }
 
