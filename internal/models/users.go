ffb063a2c8a6b9508fbfe2884a223cf2432dde8d
Ilmir Bakirov <bakirov@dev.vtb.ru>
2023-07-05 10:35:20 +0300

    implement login page

--- internal/models/users.go
+++ internal/models/users.go
@@ -2,6 +2,8 @@ package models
 
 import (
 	"context"
+	"database/sql"
+	"errors"
 	"github.com/jackc/pgx/v5/pgxpool"
 	"golang.org/x/crypto/bcrypt"
 	"strings"
@@ -42,7 +44,30 @@ VALUES ($1, $2, $3, $4)
 }
 
 func (m UserModel) Authenticate(email, password string) (int, error) {
-	return 0, nil
+	var id int
+	var hashedPassword []byte
+
+	stmt := "SELECT id, hashed_password FROM snippetbox.users WHERE email = $1"
+
+	err := m.DB.QueryRow(context.Background(), stmt, email).Scan(&id, &hashedPassword)
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return 0, ErrInvalidCredentials
+		} else {
+			return 0, err
+		}
+	}
+
+	err = bcrypt.CompareHashAndPassword(hashedPassword, []byte(password))
+	if err != nil {
+		if errors.Is(err, bcrypt.ErrMismatchedHashAndPassword) {
+			return 0, ErrInvalidCredentials
+		} else {
+			return 0, err
+		}
+	}
+
+	return id, nil
 }
 
 func (m UserModel) Exists(id int) (bool, error) {
